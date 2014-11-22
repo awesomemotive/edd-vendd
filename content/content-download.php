fa8d3c8af9e2afdab8185ad70341f1dcdec4ae6d
Sean Davis <sdavis2702@gmail.com>
2014-11-21 20:02:43 -0600

    style EDD related sidebar elements

--- content/content-download.php
+++ content/content-download.php
@@ -13,13 +13,8 @@
 			// display featured image?
 			if ( has_post_thumbnail() ) :
 				the_post_thumbnail( 'vendd_product_image', array( 'class' => 'featured-img' ) );
-			endif; 
+			endif;
+			the_content();
 		?>
-		<div class="widget product-info-wrapper">
-			<div class="product-download-buy-button">
-				<?php echo edd_get_purchase_link( array( 'id' => get_the_ID() ) ); ?>
-			</div>
-		</div>
-		<?php the_content(); ?>
 	</div>
 </article>
