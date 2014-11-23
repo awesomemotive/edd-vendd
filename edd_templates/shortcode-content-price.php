34e8b3c993154a156ee18fdc9a321eeb48182648
Sean Davis <sdavis2702@gmail.com>
2014-11-23 16:26:26 -0600

    link to product pages from [downloads] price

--- edd_templates/shortcode-content-price.php
+++ edd_templates/shortcode-content-price.php
@@ -1,7 +1,7 @@
 <?php if ( edd_has_variable_prices( get_the_ID() ) ) : ?>
-	<span class="product-price"><?php _e( 'Starting at: ', 'vendd'); edd_price( get_the_ID() ); ?></span>					
+	<a href="<?php the_permalink(); ?>"><span class="product-price"><?php _e( 'Starting at: ', 'vendd'); edd_price( get_the_ID() ); ?></span></a>				
 <?php elseif ( '0' != edd_get_download_price( get_the_ID() ) && ! edd_has_variable_prices( get_the_ID() ) ) : ?>	
-	<span class="product-price"><?php _e( 'Price: ', 'vendd' ); edd_price( get_the_ID() ); ?></span> 
+	<a href="<?php the_permalink(); ?>"><span class="product-price"><?php _e( 'Price: ', 'vendd' ); edd_price( get_the_ID() ); ?></span></a>
 <?php else : ?>
-	<span class="product-price"><?php _e( 'Free Download','vendd' ); ?></span>
+	<a href="<?php the_permalink(); ?>"><span class="product-price"><?php _e( 'Free Download','vendd' ); ?></span></a>
 <?php endif;  ?>
