7338949deabd83a3fd507040e3aaaaeec5405844
Sean Davis <sdavis2702@gmail.com>
2014-11-23 15:54:57 -0600

    add support for Subtitles plugin on EDD downloads

--- inc/edd-functions.php
+++ inc/edd-functions.php
@@ -17,3 +17,12 @@ add_filter( 'edd_download_supports', 'vendd_edd_add_comments_support' );
  * No purchase button below download content
  */
 remove_action( 'edd_after_download_content', 'edd_append_purchase_link' );
+
+	
+/*
+ * If Subtitles plugin is activated, add support for Downloads
+ */
+function vendd_subtitles() {
+	add_post_type_support( 'download', 'subtitles' );
+}
+add_action( 'init', 'vendd_subtitles' );
