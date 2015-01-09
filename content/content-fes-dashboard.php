a578e06922a67cce3287aca8a2f53ada71c62bd8
Sean Davis <sean@sdavismedia.com>
2015-01-08 21:11:23 -0600

    add new FES files

---
+++ content/content-fes-dashboard.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template used for displaying the FES dashboard
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="fes-dashboard-header">
+		<?php
+			if ( ! empty( get_theme_mod( 'vendd_fes_dashboard_title' ) ) ) {
+				echo '<h1 class="entry-title">
+						<span class="entry-title-primary">' .
+							get_theme_mod( 'vendd_fes_dashboard_title' ) .
+						'</span>';
+						$subtitle = get_post_meta( get_the_ID(), '_subtitle', true );
+						if ( class_exists( 'Subtitles' ) && ! empty( $subtitle ) ) {
+							echo '<span class="entry-subtitle">' . $subtitle . '</span>';
+						}
+				echo '</h1>';
+			} else {
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			}
+		?>
+	</header><!-- .fes-dashboard-header -->
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
---
+++ fes_templates/fes-dashboard.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Template Name: FES Dashboard
+ */
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+	
+				<?php get_template_part( 'content/content', 'fes-dashboard' ); ?>
+	
+			<?php endwhile; // end of the loop. ?>
+		
+		</main>
+	</div>
+
+<?php get_footer(); ?>
