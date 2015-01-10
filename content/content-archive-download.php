b8d27a401176ee64092f21e41cb581112eb57750
Sean Davis <sean@sdavismedia.com>
2015-01-09 19:41:54 -0600

    remove extra query code from download archive template

--- content/content-archive-download.php
+++ content/content-archive-download.php
@@ -5,21 +5,12 @@
  * all changes made here should also be made to the download taxonomy template
  * found at - templates/content-download-taxonomy.php
  */
-$current_page = get_query_var( 'paged' );
-$per_page = intval( get_theme_mod( 'vendd_store_front_count', 9 ) );
-$offset = $current_page > 0 ? $per_page * ( $current_page-1 ) : 0;
-$product_args = array(
-	'post_type'			=> 'download',
-	'posts_per_page'	=> $per_page,
-	'offset'			=> $offset
-);
-$products = new WP_Query( $product_args );
 ?>
 
 <div id="store-front">
-	<?php if ( $products->have_posts() ) : $i = 1; ?>
+	<?php if ( have_posts() ) : $i = 1; ?>
 		<div class="edd_downloads_list">
-			<?php while ( $products->have_posts() ) : $products->the_post(); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 				<div itemscope itemtype="http://schema.org/Product" class="edd_download" id="edd_download_<?php echo get_the_ID(); ?>" style="width: 33.3%; float: left;">
 					<div class="edd_download_inner">
 						<?php
@@ -36,15 +27,15 @@ $products = new WP_Query( $product_args );
 			<?php endwhile; ?>
 		</div>
 		<div style="clear:both;"></div>
-		<?php if ( $products->max_num_pages > 1 ) : ?>		
+		<?php if ( $wp_query->max_num_pages > 1 ) : ?>		
 			<div id="edd_download_pagination" class="store-pagination navigation">
 				<?php 					
 					$big = 999999999; // need an unlikely integer					
 					echo paginate_links( array(
 						'base'		=> str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 						'format'	=> '?paged=%#%',
-						'current'	=> max( 1, $current_page ),
-						'total'		=> $products->max_num_pages,
+						'current'	=> max( 1, get_query_var( 'paged' ) ),
+						'total'		=> $wp_query->max_num_pages,
 						'prev_text'	=> '<i class="fa fa-arrow-circle-left"></i> ' . __( 'Previous', 'vendd' ),
 						'next_text'	=> __( 'Next', 'vendd' ) . ' <i class="fa fa-arrow-circle-right"></i>'
 					) );
