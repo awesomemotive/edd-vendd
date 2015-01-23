dd83263079471fa1acaac802366f69b51fe91942
Sean Davis <sean@sdavismedia.com>
2015-01-22 17:47:27 -0700

    style non-store front [downloads] shortcode styles

--- edd_templates/shortcode-content-cart-button.php
+++ edd_templates/shortcode-content-cart-button.php
@@ -2,5 +2,13 @@
 /**
  * replace the purchase button
  */
-?>
-<a class="product-link" href="<?php the_permalink(); ?>"><i class="fa fa-arrow-circle-right"></i></a>
+
+if ( vendd_is_store_front() ) { ?>
+	<a class="product-link" href="<?php the_permalink(); ?>"><i class="fa fa-arrow-circle-right"></i></a>
+	<?php
+} else { ?>
+	<div class="edd_download_buy_button">
+		<?php echo edd_get_purchase_link( array( 'download_id' => get_the_ID() ) ); ?>
+	</div>
+	<?php
+}
