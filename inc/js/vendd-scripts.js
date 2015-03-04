492c410a4ee02af291300fc8e539fe351385c5e3
Sean Davis <sean@sdavismedia.com>
2015-03-04 14:42:43 -0600

    adjust variable names

--- inc/js/vendd-scripts.js
+++ inc/js/vendd-scripts.js
@@ -1,12 +1,12 @@
 ( function( $ ) {
 	$( ".vendd-show-button" ).on( "click", function(e){
-		var postList = $( this ).siblings( ".vendd-price-button-container" );
-		if ( postList.css( "display" ) == "none" ) {
-			postList.slideDown();
-			$(this).addClass( "vendd-price-button-displayed" );
-		} else if (postList.css( "display" ) == "block" ) {
-			postList.slideUp();
-			$(this).removeClass( "vendd-price-button-displayed" );
+		var purchaseButton = $( this ).siblings( ".vendd-price-button-container" );
+		if ( purchaseButton.css( "display" ) == "none" ) {
+			purchaseButton.slideDown();
+			$( this ).addClass( "vendd-price-button-displayed" );
+		} else if (purchaseButton.css( "display" ) == "block" ) {
+			purchaseButton.slideUp();
+			$( this ).removeClass( "vendd-price-button-displayed" );
 		}
 		return false;
 	});
