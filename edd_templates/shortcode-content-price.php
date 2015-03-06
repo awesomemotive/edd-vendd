8a53fc79b3c3d2c30e47bf5e1f2251c7f563291c
Sean Davis <sean@sdavismedia.com>
2015-03-05 19:33:32 -0600

    remove white space

--- edd_templates/shortcode-content-price.php
+++ edd_templates/shortcode-content-price.php
@@ -4,4 +4,4 @@
 	<a class="product-price-wrap" href="<?php the_permalink(); ?>"><span class="product-price"><?php _e( 'Price: ', 'vendd' ); edd_price( get_the_ID() ); ?></span></a>
 <?php else : ?>
 	<a class="product-price-wrap" href="<?php the_permalink(); ?>"><span class="product-price"><?php _e( 'Free Download','vendd' ); ?></span></a>
-<?php endif;  ?>
+<?php endif; ?>
