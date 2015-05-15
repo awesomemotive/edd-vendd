0a17e82d774879b5113f8f10b80440f2a2a71934
Sean Davis <sean@sdavismedia.com>
2015-05-15 11:23:11 -0700

    fix download category/tag archives

--- content/content-archive-download.php
+++ content/content-archive-download.php
@@ -6,8 +6,50 @@
 if ( have_posts() ) : $i = 1;
 	?>
 	<div id="store-front">
-		<?php echo do_shortcode( '[downloads]' ); ?>
-	</div>
+		<div class="edd_downloads_list edd_download_columns_3">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<div itemscope itemtype="http://schema.org/Product" class="edd_download" id="edd_download_<?php echo get_the_ID(); ?>">
+					<div class="edd_download_inner">
+						<?php
+							/**
+							 * These are the same template files used by the [downloads] shortcode.
+							 * So making adjustments to those template files will affect this archive
+							 * as well as the [downloads] shortcode.
+							 *
+							 * To make adjustments specifically to archive template download output,
+							 * grab the contents of the relevant template file and put it in place of the
+							 * appropriate call below that way your changes are focused here and not the
+							 * [downloads] shortcode... unless that's what you want.
+							 */
+							edd_get_template_part( 'shortcode', 'content-image' );
+							edd_get_template_part( 'shortcode', 'content-title' );
+							edd_get_template_part( 'shortcode', 'content-excerpt' );
+							edd_get_template_part( 'shortcode', 'content-price' );
+							edd_get_template_part( 'shortcode', 'content-cart-button' );
+						?>
+					</div>
+				</div>
+				<?php if ( $i % 3 == 0 ) { ?><div style="clear:both;"></div><?php } ?>
+				<?php $i+=1; ?>
+			<?php endwhile; ?>
+		</div>
+		<div style="clear:both;"></div>
+		<?php if ( $wp_query->max_num_pages > 1 ) : ?>		
+			<div id="edd_download_pagination" class="store-pagination navigation">
+				<?php 					
+					$big = 999999999; // need an unlikely integer					
+					echo paginate_links( array(
+						'base'		=> str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+						'format'	=> '?paged=%#%',
+						'current'	=> max( 1, get_query_var( 'paged' ) ),
+						'total'		=> $wp_query->max_num_pages,
+						'prev_text'	=> __( '&laquo; Previous', 'vendd' ),
+						'next_text'	=> __( 'Next  &raquo;', 'vendd' ),
+					) );
+				?>
+			</div>
+		<?php endif; ?>
+	</div><!-- #store-front -->
 	<?php
 else :
 	?>
