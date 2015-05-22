5efa824d21b53aeb96c58bf0a08605fe1dc4eaa2
Sean Davis <sean@sdavismedia.com>
2015-05-21 23:30:11 -0500

    hide customizer control descriptions behind toggle link

--- inc/js/vendd-scripts.js
+++ inc/js/vendd-scripts.js
@@ -3,26 +3,19 @@
  *
  * @package Vendd
  */
-( function( $ ) {
-
-	$(document).ready(function() {
-
-		$('.vendd-show-button').on('click', function(e){
+(function($){
+	$(document).ready(function(){
+		$('.vendd-show-button').on('click',function(e){
 			e.preventDefault();
 			$(this).toggleClass('vendd-price-button-displayed').siblings('.vendd-price-button-container').slideToggle();
 		});
-
 		var body = $(document.body);
 		var header_cart_total = $('.header-cart-total');
-
-		body.on('edd_cart_item_added', function (event, response) {
+		body.on('edd_cart_item_added',function(event, response){
 			header_cart_total.html(response.subtotal);
 		});
-
-		body.on('edd_cart_item_removed', function (event, response) {
+		body.on('edd_cart_item_removed',function(event, response){
 			header_cart_total.html (response.subtotal);
 		});
-
 	});
-
 })(jQuery);
