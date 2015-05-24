ad7f1a87563935f77e134b421dec99042cfb609d
Sean Davis <sean@sdavismedia.com>
2015-05-24 13:40:43 -0500

    add Ninja Form support

--- content/content-members.php
+++ content/content-members.php
@@ -6,7 +6,7 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'vendd-edd-fes-shortcode' ); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header>
