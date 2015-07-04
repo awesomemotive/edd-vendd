9090e359df0655f1f02264f2de3678c688ba9b7e
Sean Davis <sean@sdavismedia.com>
2015-07-04 16:15:37 -0500

    clean up JS

--- inc/js/customizer.js
+++ inc/js/customizer.js
@@ -5,16 +5,16 @@
  *
  * @package Vendd
  */
-( function( $ ) {
+(function($){
 	// Site title and description.
-	wp.customize( 'blogname', function( value ) {
-		value.bind( function( to ) {
-			$( '.site-title a' ).text( to );
-		} );
-	} );
-	wp.customize( 'blogdescription', function( value ) {
-		value.bind( function( to ) {
-			$( '.site-description' ).text( to );
-		} );
-	} );
-} )( jQuery );
+	wp.customize('blogname',function(value){
+		value.bind(function(to){
+			$('.site-title a').text(to);
+		});
+	});
+	wp.customize('blogdescription',function(value){
+		value.bind(function(to){
+			$('.site-description').text(to);
+		});
+	});
+})(jQuery);
