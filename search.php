aec6238f590c956db160095b81d3b4b865786ff8
Sean Davis <sean@sdavismedia.com>
2015-07-05 02:19:02 -0500

    enchance search results template

--- search.php
+++ search.php
@@ -4,33 +4,38 @@
  *
  * @package Vendd
  */
-
+$advanced_search = get_theme_mod( 'vendd_advanced_search_results', 0 );
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<section id="primary" class="content-area<?php $advanced_search ? 'advanced-search-content' : ''; ?>">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'vendd' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				<h1 class="page-title"><?php printf( __( 'Search results for: %s', 'vendd' ), '<span><strong>' . get_search_query() . '</strong></span>' ); ?></h1>
 			</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php
+				if ( ! $advanced_search ) :
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
+	
+	
+					/**
+					 * Run the loop for the search to output the results.
+					 * If you want to overload this in a child theme then include a file
+					 * called content-search.php and that will be used instead.
+					 */
+					get_template_part( 'content/content', 'search' );
+	
+					endwhile;
+				else :
+					get_template_part( 'content/content', 'advanced-search' );
+				endif;
+			?>
 
-				<?php
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'content/content', 'search' );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php vendd_paging_nav(); ?>
+			<?php ! $advanced_search ? vendd_paging_nav() : ''; ?>
 
 		<?php else : ?>
 
@@ -41,5 +46,5 @@ get_header(); ?>
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
-<?php get_sidebar(); ?>
+<?php ! $advanced_search ? get_sidebar() : ''; ?>
 <?php get_footer(); ?>
