5fb656b70791a0d7bb15da844e61157cd113a176
Sean Davis <seandavis@Seans-iMac.attlocal.net>
2017-02-12 12:43:13 -0800

    add setting to show/hide featured image on products #193

--- content/content-download.php
+++ content/content-download.php
@@ -13,13 +13,15 @@
 	<div class="entry-content">
 		<?php
 			// display featured image? if not, display default featured image?
-			if ( has_post_thumbnail() ) :
-				$product_image = apply_filters( 'vendd_crop_product_image', true ) ? 'vendd_product_image' : 'full';
-				the_post_thumbnail( $product_image, array( 'class' => 'featured-img' ) );
-			elseif ( get_theme_mod( 'vendd_product_image_upload' ) && get_theme_mod( 'vendd_product_image' ) ) :
-				?>
-				<img class="featured-img" src="<?php echo get_theme_mod( 'vendd_product_image_upload' ); ?>" alt="<?php echo esc_attr( get_the_title( get_the_ID() ) ); ?>">
-				<?php
+			if (  1 == get_theme_mod( 'vendd_product_featured_image', 1 ) ) :
+				if ( has_post_thumbnail() ) :
+					$product_image = apply_filters( 'vendd_crop_product_image', true ) ? 'vendd_product_image' : 'full';
+					the_post_thumbnail( $product_image, array( 'class' => 'featured-img' ) );
+				elseif ( get_theme_mod( 'vendd_product_image_upload' ) && get_theme_mod( 'vendd_product_image' ) ) :
+					?>
+					<img class="featured-img" src="<?php echo get_theme_mod( 'vendd_product_image_upload' ); ?>" alt="<?php echo esc_attr( get_the_title( get_the_ID() ) ); ?>">
+					<?php
+				endif;
 			endif;
 			the_content();
 		?>
