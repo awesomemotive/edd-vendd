3a71f783f99a1dd558e05ee272c5485d7f2ec46c
Chris Klosowski <cklosowski@gmail.com>
2022-01-11 11:43:32 -0700

    Version bump for 1.2.6

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
  */
 define( 'VENDD_NAME', 'Vendd' );
 define( 'VENDD_AUTHOR', 'Sandhills Development, LLC' );
-define( 'VENDD_VERSION', '1.2.5' );
+define( 'VENDD_VERSION', '1.2.6' );
 define( 'VENDD_HOME', 'https://easydigitaldownloads.com/downloads/vendd' );
 
 
